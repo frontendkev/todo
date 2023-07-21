5eaed94e5e2562642cbb275b659a1bc4124897b4
frontendkev <kevfrontdev@gmail.com>
2023-07-21 14:36:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,37 @@ import ToDo from "./Components/ToDo/ToDo";
 import Header from "./Components/Header/Header";
 import banner from './images/banner.jpg'
 import TodoProvider from "./Reducer/Reducer";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 export default function App() {
+
+    
+
+    const Root = () => {
+        return (
+            <>
+                <header className="relative w-full h-[6vh] md:h-[10vh] lg:h-[10vh] landscape:h-[10vh] bg-mynavyblue pl-4 landscape:2xl:bg-red-500">
+                    <Header />
+                </header>
+                <main className="relative w-full h-[94vh] md:h-[90vh] overflow-hidden">
+                    <TodoProvider>
+                        <BackgroundImage image={banner} />
+                        <ToDo />
+                    </TodoProvider>
+                </main>
+            </>
+        )
+    }
+
+    const routes = createBrowserRouter(
+        createRoutesFromElements(
+            <Route index path="/" element={ <Root /> } />
+        )
+    )
     
     return (
         <div className="App">
-            <header className="relative w-full h-[6vh] md:h-[10vh] lg:h-[10vh] landscape:h-[10vh] bg-mynavyblue pl-4 landscape:2xl:bg-red-500">
-                <Header />
-            </header>
-            <main className="relative w-full h-[94vh] md:h-[90vh] overflow-hidden">
-                <TodoProvider>
-                    <BackgroundImage image={banner} />
-                    <ToDo />
-               </TodoProvider>
-            </main>
+            <RouterProvider router={routes} />
         </div>
     )
 }
